dce554fd0c3887af4d24260c45eb22f5b09ca264
林小龙 <xiaolong.lin@ele.me>
2018-04-18 15:55:06 +0800

    feat: change addressAddOrUpdate to gotoAddressEdit

--- pages/shopping/address/address.js
+++ pages/shopping/address/address.js
@@ -1,18 +1,50 @@
 // pages/shopping/address/address.js
+import util from '../../../utils/util.js';
+import api from '../../../config/api.js';
+// 在微信小程序使用async,await需要引入regeneratorRuntime变量，具体请看https://ninghao.net/blog/5508
+import regeneratorRuntime from '../../../libs/regenerator-runtime';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    addressList: []
   },
+  /**
+   * 获取地址列表
+   */
+  async getAddressList() {
+    const addressList = await util.request(api.AddressList);
+    this.setData({ addressList });
+  },
+  /**
+   * 跳转至地址修改页
+   */
+  gotoAddressEdit(event) {
+    const { addressId } = event.currentTarget.dataset;
+    wx.navigateTo({
+      url: `/pages/shopping/addressAdd/addressAdd?id=${addressId}`
+    });
+  },
+  /**
+   * 选中地址
+   */
+  selectAddress(event) {
+    try {
+      const { addressId } = event.currentTarget.dataset;
+      wx.setStorageSync('shopping.addressId', addressId);
+    } catch(e) {
+      console.log('shopping address error: ', e);
+    }
 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getAddressList();
   },
 
   /**
